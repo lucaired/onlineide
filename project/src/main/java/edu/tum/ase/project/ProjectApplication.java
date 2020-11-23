224cb55220cd0617b8a4205c95e75ec4b8802657
Luca A. Müller <luca.mueller@cs.tum.edu>
2020-11-23 16:54:54 +0100

    add postgres persistence for project

--- project/src/main/java/edu/tum/ase/project/ProjectApplication.java
+++ project/src/main/java/edu/tum/ase/project/ProjectApplication.java
@@ -1,13 +1,39 @@
 package edu.tum.ase.project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.sql.DataSource;
+
+import edu.tum.ase.project.service.ProjectService;
+import edu.tum.ase.project.model.Project;
+import java.util.List;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(ProjectApplication.class);
+
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	private ProjectService projectService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("DataSource = " + dataSource);
+		Project project = projectService.createProject(new Project("my-project"));
+		log.info("ID of saved project = " + project.getId());
+		Project p = projectService.findByName("my-project");
+		log.info("ID of queried project = " + p.getId());
+		List<Project> projects = projectService.getProjects();
+		log.info("Length of project list = " + projects.size());
+	}
 }
--- project/src/main/java/edu/tum/ase/project/service/ProjectService.java
+++ project/src/main/java/edu/tum/ase/project/service/ProjectService.java
@@ -1,2 +1,27 @@
-package edu.tum.ase.project.service;public class ProjectService {
+package edu.tum.ase.project.service;
+
+import edu.tum.ase.project.model.Project;
+import edu.tum.ase.project.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public Project findByName(String name) {
+        return projectRepository.findByName(name);
+    }
+
+
+    public List<Project> getProjects() {
+        return projectRepository.findAll();
+    }
 }
