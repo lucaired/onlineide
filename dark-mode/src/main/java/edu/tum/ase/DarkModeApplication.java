2798070aa61bea5799f15bbaaed8f928ab80a829
Adrian Mitter <adrian.mitter@tum.de>
2021-01-29 14:26:31 +0100

    Turn darkmode microservice into Eureka client

--- dark-mode/src/main/java/edu/tum/ase/DarkModeApplication.java
+++ dark-mode/src/main/java/edu/tum/ase/DarkModeApplication.java
@@ -2,8 +2,10 @@ package edu.tum.ase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DarkModeApplication {
 
     public static void main(String[] args) {
