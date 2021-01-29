11d128ca807b3fa80fb0b7fbcd928e517f2b8fb1
Adrian Mitter <adrian.mitter@tum.de>
2021-01-29 14:07:48 +0100

    Configure Eureka discovery server

--- service-registry/src/main/java/edu/tum/ase/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/edu/tum/ase/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package edu.tum.ase.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
