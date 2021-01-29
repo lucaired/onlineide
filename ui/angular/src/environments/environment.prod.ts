2c5890909cac5aab99d0ba7d58b1c60ebac7e869
Adrian Mitter <adrian.mitter@tum.de>
2021-01-29 14:32:25 +0100

    Add ui service to gateway

--- ui/angular/src/environments/environment.prod.ts
+++ ui/angular/src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
+// By using requests like /api/projects instead of full URLs with localhost, we send the requests directly to the gateway.
+// Since the gateway also serves this frontend, the browser uses the host and appends the part like /api/projects.
+// The gateway then processes the incoming request and forwards it internally to the project microservice.
+// So this frontend here does not communicate directly with the microservices, but always with the gateway.
 export const environment = {
   production: true,
   api: {
-    projects: 'http://localhost:8100/api/projects',
-    compiler: 'http://localhost:8200/compile',
-    darkMode: 'http://localhost:8300/dark-mode'
+    projects: '/api/projects',
+    compiler: '/api/compile',
+    darkMode: '/api/dark-mode'
   }
 };
--- ui/angular/src/environments/environment.ts
+++ ui/angular/src/environments/environment.ts
@@ -1,8 +1,11 @@
+// Unlike the production environment, for development purposes we use the addresses of the actual microservices without service discovery and gateway.
+// This makes local development a bit more lightweight because not everything has to be set up for Eureka and the Zuul Gateway.
+// However, for the final local testing of a feature, the production environment should then be used.
 export const environment = {
   production: false,
   api: {
     projects: 'http://localhost:8100/api/projects',
-    compiler: 'http://localhost:8200/compile',
-    darkMode: 'http://localhost:8300/dark-mode'
+    compiler: 'http://localhost:8200/api/compile',
+    darkMode: 'http://localhost:8300/api/dark-mode'
   }
 };
