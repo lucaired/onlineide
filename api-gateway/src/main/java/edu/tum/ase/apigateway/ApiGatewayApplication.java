76d1e405230279881a60873dcc4ca4c92fcca4d4
Adrian Mitter <adrian.mitter@tum.de>
2021-01-29 14:30:59 +0100

    Configure API gateway

--- api-gateway/src/main/java/edu/tum/ase/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/edu/tum/ase/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package edu.tum.ase.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
