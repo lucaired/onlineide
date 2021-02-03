90220a0203a2b59f1517f08858be4feee5a2334c
Matthias Unterfrauner <matthias.unterfrauner@tum.de>
2021-02-03 16:08:18 +0100

    Re-enable Auth Guard

--- ui/angular/src/app/services/auth/auth.guard.ts
+++ ui/angular/src/app/services/auth/auth.guard.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/router';
 import {AuthService} from './auth.service';
 import {Observable, of} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
@@ -20,16 +21,16 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): Observable<boolean> {
-    return of(true);
-    // return this.authService.isAuthenticated$.pipe(
-    //   map((isAuthenticated) => {
-    //     if (isAuthenticated) {
-    //       return true;
-    //     } else {
-    //       this.router.navigate(['login']);
-    //       return false;
-    //     }
-    //   }));
+    // return of(true);
+    return this.authService.isAuthenticated$.pipe(
+      map((isAuthenticated) => {
+        if (isAuthenticated) {
+          return true;
+        } else {
+          this.router.navigate(['login']);
+          return false;
+        }
+      }));
   }
 
 }
