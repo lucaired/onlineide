af2009aa6e4921a6c0480619156e39fe1261cb30
Adrian Mitter <adrian.mitter@tum.de>
2021-02-05 19:02:26 +0100

    Remove error-handling package

--- error-handling/src/main/java/edu/tum/ase/errorhandling/exception/ResourceAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package edu.tum.ase.errorhandling.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException{
-
-    public ResourceAlreadyExistsException(Class c, String field, String value){
-        super(c.getSimpleName() + " with " + field + " '" + value + "' already exists");
-    }
-}
